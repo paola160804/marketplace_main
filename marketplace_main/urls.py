6c225c1f3a6abd0d014687d8cede53b629e0dbdb
paola160804 <paolacova99@gmail.com>
2025-11-11 21:03:23 -0600

    agregue el forms

--- marketplace_main/urls.py
+++ marketplace_main/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls. static import static
 from store.views import home
@@ -23,6 +23,7 @@ from store.views import home
 urlpatterns = [
     path('', home, name='home'),
     path('admin/', admin.site.urls),
+    path('store/', include('store.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
